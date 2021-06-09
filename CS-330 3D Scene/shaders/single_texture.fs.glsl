135a70b45653da2669c669b6387983f2edbf83be
Griffin Nelson <griffin.nelson@snhu.edu>
2021-06-09 15:14:26 -0400

    added ambient and diffuse lighting to shaders for single texture objects

--- CS-330 3D Scene/shaders/single_texture.fs.glsl
+++ CS-330 3D Scene/shaders/single_texture.fs.glsl
@@ -1,10 +1,31 @@
 #version 330 core
 in vec2 TexCoord;
+in vec3 FragPos;
+in vec3 Normal;
 out vec4 FragColor;
 
+uniform vec3 lightPos;
+uniform vec3 lightColor;
 uniform sampler2D texture;
 
 void main()
 {
-	FragColor = texture(texture, TexCoord);
+	// calculate ambient lighting
+	float ambient_strength = 0.1;
+	vec3 ambient_color = vec3(1.0, 1.0, 1.0);			// assume ambient lighting to be white (typical)
+	vec3 ambient = ambient_color * ambient_strength;
+
+	// calculate diffuse lighting
+	vec3 norm = normalize(Normal);						// normalize Normal vector incase does not already
+															// have a magnitude of 1
+	vec3 lightDir = normalize(lightPos - FragPos);		// calculate direction of ray hitting fragment and
+															// normalize the result
+	float diff = max(dot(norm, lightDir), 0.0);			// calculate how bright the fragment should be based
+															// on the angle between the normal and ray of light
+	vec3 diffuse = diff * lightColor;
+
+	// calculate specular lighting
+
+	// calculate fragment color
+	FragColor = vec4(ambient + diffuse, 1.0) * texture(texture, TexCoord);
 }
--- CS-330 3D Scene/shaders/single_texture.vs.glsl
+++ CS-330 3D Scene/shaders/single_texture.vs.glsl
@@ -4,6 +4,9 @@ layout (location = 1) in vec3 aNorm;
 layout (location = 2) in vec2 aTexCoord;
 
 out vec2 TexCoord;																	// Define the output parameter (taken by the fragment shader to texture the fragment).
+out vec3 FragPos;
+out vec3 Normal;
+
 uniform mat4 model;																	// Model matrix (uniform input)
 uniform mat4 view;																	// View matrix (uniform input)
 uniform mat4 projection;															// Projection matrix (uniform input)
@@ -11,4 +14,6 @@ void main()
 {
 	gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);	// Map the input vec3 to a vec4 and set it to gl_Position. 
 	TexCoord = aTexCoord;
+	FragPos = vec3(model * vec4(aPos, 1.0));
+	Normal = aNorm;
 }
