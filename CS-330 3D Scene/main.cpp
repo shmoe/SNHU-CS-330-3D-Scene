6e6efc5c67820e45e4aa29188e74540e70484fd6
Griffin Nelson <griffin.nelson@snhu.edu>
2021-06-11 20:38:54 -0400

    formatting

--- CS-330 3D Scene/main.cpp
+++ CS-330 3D Scene/main.cpp
@@ -213,13 +213,13 @@ int main(int argc, char* argv[]) {
 		/**
 		 * Draw models
 		 */
-		draw_radiant_light(light, projection, view);							// Draw light source
+		draw_radiant_light(light, projection, view);																// Draw light source
 
-		draw_model(desk, projection, view, light.position, light.color, glob::cameraPos);		// Draw desk Model
+		draw_model(desk, projection, view, light.position, light.color, glob::cameraPos);							// Draw desk Model
 		draw_material_model(console, console_mat, projection, view, light.position, light.color, glob::cameraPos);	// Draw console Model
-		draw_model(napkin, projection, view, light.position, light.color, glob::cameraPos);		// Draw napkin Model
-		draw_model(orange, projection, view, light.position, light.color, glob::cameraPos);		// Draw orange Model
-		draw_model(soda, projection, view, light.position, light.color, glob::cameraPos);		// Draw soda can Model
+		draw_model(napkin, projection, view, light.position, light.color, glob::cameraPos);							// Draw napkin Model
+		draw_model(orange, projection, view, light.position, light.color, glob::cameraPos);							// Draw orange Model
+		draw_model(soda, projection, view, light.position, light.color, glob::cameraPos);							// Draw soda can Model
 
 
 		glfwSwapBuffers(window);				// Swaps front and back framebuffers (output to screen)
