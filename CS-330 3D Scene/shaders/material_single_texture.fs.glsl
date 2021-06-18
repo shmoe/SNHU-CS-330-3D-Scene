45c62a07aced9aad683aa800836c5e6162225c9e
Griffin Nelson <griffin.nelson@snhu.edu>
2021-06-18 19:20:10 -0400

    implemented directional lighting in frag shaders

--- CS-330 3D Scene/shaders/material_single_texture.fs.glsl
+++ CS-330 3D Scene/shaders/material_single_texture.fs.glsl
@@ -5,7 +5,6 @@ in vec3 Normal;
 out vec4 FragColor;
 
 uniform float ambientStrength;
-uniform vec3 ambientColor;
 
 struct PointLight {
 	vec3 position;
@@ -32,21 +31,22 @@ vec3 CalcPointLight(PointLight light) {
 	float attenuation = 1.0 / (attenCoeff.x + attenCoeff.y * lightDistance + attenCoeff.z * (lightDistance * lightDistance));
 
 	// calculate ambient lighting
-	vec3 ambient = ambientColor * ambientStrength;
+	vec3 ambient = light.color * ambientStrength;
 
 	// calculate diffuse lighting
-	vec3 norm = normalize(Normal);						// normalize Normal vector incase does not already
-															// have a magnitude of 1
+	vec3 norm = normalize(Normal);								// normalize Normal vector incase does not already
+																	// have a magnitude of 1
 	vec3 lightDir = normalize(light.position - FragPos);		// calculate direction of ray hitting fragment and
-															// normalize the result
-	float diff = max(dot(norm, lightDir), 0.0);			// calculate how bright the fragment should be based
-															// on the angle between the normal and ray of light
+																	// normalize the result
+	float diff = max(dot(norm, lightDir), 0.0);					// calculate how bright the fragment should be based
+																	// on the angle between the normal and ray of light
 	vec3 diffuse = diff * light.color;
 	
 	// calculate specular lighting
 	vec3 viewDir = normalize(viewPos - FragPos);				// calculate view direction and normalize
 	vec3 reflectDir = reflect(-lightDir, norm);					// calculate direction of reflected light
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);	// calculate specular constant
+
 	vec3 specular = specularStrength * spec * light.color * vec3(texture(specularMap, TexCoord));
 
 	// adjust for attenuation
@@ -56,8 +56,32 @@ vec3 CalcPointLight(PointLight light) {
 	return (ambient + diffuse + specular);
 }
 
+vec3 CalcDirLight(DirectionalLight light){
+	// calculate light direction
+	vec3 lightDir = normalize(-light.direction);
+
+	// calculate ambient lighting
+	vec3 ambient = light.color * ambientStrength;
+
+	// calculate diffuse lighting
+	vec3 norm = normalize(Normal);								// normalize Normal vector incase does not already
+																	// have a magnitude of 1
+	float diff = max(dot(norm, lightDir), 0.0);					// calculate how bright the fragment should be based
+																	// on the angle between the normal and ray of light
+	vec3 diffuse = diff * light.color;
+
+	// calculate specular lighting
+	vec3 viewDir = normalize(viewPos - FragPos);				// calculate view direction and normalize
+	vec3 reflectDir = reflect(-lightDir, norm);					// calculate direction of reflected light
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);	// calculate specular constant
+
+	vec3 specular = specularStrength * spec * light.color * vec3(texture(specularMap, TexCoord));
+
+	return (ambient + diffuse + specular);
+}
+
 void main()
 {
 	// calculate fragment color
-	FragColor = vec4(CalcPointLight(pointLight), 1.0) * texture(aTexture, TexCoord);
+	FragColor = vec4(CalcPointLight(pointLight) + CalcDirLight(dirLight), 1.0) * texture(aTexture, TexCoord);
 }
